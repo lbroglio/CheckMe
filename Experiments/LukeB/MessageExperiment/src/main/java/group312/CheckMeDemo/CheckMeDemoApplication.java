7f7f9a68053f7ecc66996a1b011b68f36a032078
lbroglio <lbroglio@iastate.edu>
2023-08-30 15:23:17 -0500

    Intial Commit - Files from Spring initialzier, Message Class, Start of MessageRetriever

---
+++ Experiments/LukeB/MessageExperiment/src/main/java/group312/CheckMeDemo/CheckMeDemoApplication.java
@@ -0,0 +1,16 @@
+package group312.CheckMeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class CheckMeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckMeDemoApplication.class, args);
+	}
+
+	@RequestMapping
+
+}
---
+++ Experiments/LukeB/MessageExperiment/src/test/java/group312/CheckMeDemo/CheckMeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package group312.CheckMeDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckMeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
