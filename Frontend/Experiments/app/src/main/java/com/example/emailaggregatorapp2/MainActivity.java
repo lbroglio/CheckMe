ffcdd1169e88be6a3543f7b169b30fb8949c5088
joelbrickey <jbrickey@iastate.edu>
2023-10-02 22:45:32 -0500

    Frontend changes

---
+++ Frontend/Experiments/app/src/main/java/com/example/emailaggregatorapp2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.emailaggregatorapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginbut = (Button) findViewById(R.id.loginbut1);
+        Button signupbut = (Button) findViewById(R.id.signupbut1);
+
+        signupbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
