5902c1aeac5b57dd82b36cd5aeaa29ec81eb6be6
joelbrickey <jbrickey@iastate.edu>
2023-10-05 15:40:01 -0500

    Frontend JsonObject edits

--- Frontend/Experiments/app/src/main/java/com/example/emailaggregatorapp2/MainActivity.java
+++ Frontend/Experiments/app/src/main/java/com/example/emailaggregatorapp2/MainActivity.java
@@ -7,6 +7,26 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -33,4 +53,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    /**
+    private void makeJsonObjectRequest() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                API_URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Handle the successful response here
+                        responseTextView.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle any errors that occur
+                        responseTextView.setText("Error: " + error.toString());
+                    }
+                }
+        );
+     */
 }
