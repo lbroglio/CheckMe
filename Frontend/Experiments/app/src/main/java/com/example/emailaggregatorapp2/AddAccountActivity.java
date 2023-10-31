4a8f27dc67c104580df661bc9d0f97480439a747
lbroglio <lbroglio@iastate.edu>
2023-10-31 16:28:42 -0500

    Started screen for adding retrievers

--- Frontend/Experiments/app/src/main/java/com/example/emailaggregatorapp2/AddAccountActivity.java
+++ Frontend/Experiments/app/src/main/java/com/example/emailaggregatorapp2/AddAccountActivity.java
@@ -1,17 +1,9 @@
 package com.example.emailaggregatorapp2;
 
-import android.content.Intent;
-import android.opengl.Visibility;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.Group;
-
-import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -22,50 +14,13 @@ import com.android.volley.toolbox.StringRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MessagesActivity extends AppCompatActivity{
+public class AddAccountActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_messages);
+        setContentView(R.layout.activity_add_acount);
 
-        //TODO - Rework for Message frontend
-
-
-        // Navigation Bar
-
-        //Group for all Navbar Objects
-        Group navBar = (Group)  findViewById(R.id.NavBarGroup);
-
-        // Button for toggling Visibility
-        ToggleButton visButton = (ToggleButton) findViewById(R.id.navBarToggle);
-
-        // Button for going to accounts page
-        Button accountButton = (Button) findViewById(R.id.TPAccountsGoTo);
-
-
-        // Toggle navbar visibility
-        visButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(navBar.getVisibility() == View.GONE){
-                    navBar.setVisibility(View.VISIBLE);
-                }
-                else{
-                    navBar.setVisibility(View.GONE);
-
-                }
-            }
-        });
-
-        // Go to third party accounts Screen
-        accountButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MessagesActivity.this, AddAccountActivity.class);
-                startActivity(intent);
-            }
-        });
 
 
     }
