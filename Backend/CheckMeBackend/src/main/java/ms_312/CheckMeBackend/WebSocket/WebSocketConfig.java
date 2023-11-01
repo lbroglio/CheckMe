37c0a8ddd0ca17970a8524e054d564876d14f5de
josh <josh@rarceo.com>
2023-11-01 14:28:04 -0500

    Added WebSocketConfig

--- Backend/CheckMeBackend/src/main/java/ms_312/CheckMeBackend/WebSocket/WebSocketConfig.java
+++ Backend/CheckMeBackend/src/main/java/ms_312/CheckMeBackend/WebSocket/WebSocketConfig.java
@@ -1,7 +1,15 @@
 package ms_312.CheckMeBackend.WebSocket;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
 
 public class WebSocketConfig {
 
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
 
 }
