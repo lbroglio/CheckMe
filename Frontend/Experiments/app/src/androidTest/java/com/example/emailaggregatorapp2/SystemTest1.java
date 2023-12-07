2408abb16d23d9bd51afd80c464f9aa9d15b8d0d
josh <josh@rarceo.com>
2023-12-07 01:25:37 -0600

    Finished Test Cases for signup

--- Frontend/Experiments/app/src/androidTest/java/com/example/emailaggregatorapp2/SystemTest1.java
+++ Frontend/Experiments/app/src/androidTest/java/com/example/emailaggregatorapp2/SystemTest1.java
@@ -1,8 +1,7 @@
 package com.example.emailaggregatorapp2;
 
 
-import com.example.emailaggregatorapp2.MainActivity;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,8 +19,13 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.test.rule.ActivityTestRule;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
@@ -29,41 +33,113 @@ import androidx.test.rule.ActivityTestRule;
 public class SystemTest1 {
     private static final int SIMULATED_DELAY_MS = 500;
 
+    String testUser = "testUser" + (int)(Math.random() * 100000);
+    String email = testUser + "@gmail.com";
+    String password = "password";
 
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
+
     @Before
     public void setUp(){
         Intents.init();
+//        activityRule.launchActivity(null);
     }
 
 
+    @After
+    public void tearDown(){
+        Intents.release();
+    }
     @Test
-    public void signUpSuccess(){
+    public void signUpSuccessandLogin(){
         //generate random username
-        String testUser = "testUser" + (int)(Math.random() * 1000);
-        String email = testUser + "@gmail.com";
-        String password = "password";
-
 
 
         //input info into sign up page, then see if activity changes to login page or if an error message pops up
-        onView(ViewMatchers.withId(R.id.signupbut1)).perform(click());
-        onView(ViewMatchers.withId(R.id.usernameInp)).perform(typeText(testUser), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.emailInp)).perform(typeText(email), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.passInp)).perform(typeText(password), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.passConfirmInp)).perform(typeText(password), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.loginbut2)).perform(click());
+        onView(withId(R.id.signupbut1)).perform(click());
+        onView(withId(R.id.usernameInp)).perform(typeText(testUser), closeSoftKeyboard());
+        onView(withId(R.id.emailInp)).perform(typeText(email), closeSoftKeyboard());
+        onView(withId(R.id.passInp)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.passConfirmInp)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.loginbut2)).perform(click());
 
         try {
             Thread.sleep(SIMULATED_DELAY_MS);
         } catch (InterruptedException e) {
         }
 
-        intended(hasComponent(LoginActivity.class.getName()));
+//        intended(hasComponent(LoginActivity.class.getName()));
+
+
+        onView(withId(R.id.loginusernameedittext)).perform(typeText(testUser), closeSoftKeyboard());
+        onView(withId(R.id.loginpasswordedittext)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.loginbut2)).perform(click());
+
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+
+        intended(hasComponent(MessagesActivity.class.getName()));
+
 
     }
 
+    @Test
+    public void signUpFailPassMisMatch() {
+        onView(withId(R.id.signupbut1)).perform(click());
+        onView(withId(R.id.usernameInp)).perform(typeText(testUser), closeSoftKeyboard());
+        onView(withId(R.id.emailInp)).perform(typeText(email), closeSoftKeyboard());
+        onView(withId(R.id.passInp)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.passConfirmInp)).perform(typeText(password+"ABC"), closeSoftKeyboard());
+        onView(withId(R.id.loginbut2)).perform(click());
+
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+
+        onView(withText("Passwords do not match")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button1)).perform(click());
+
+    }
+
+    @Test
+    public void signUpFailUserExists() {
+        onView(withId(R.id.signupbut1)).perform(click());
+        onView(withId(R.id.usernameInp)).perform(typeText(testUser), closeSoftKeyboard());
+        onView(withId(R.id.emailInp)).perform(typeText(email), closeSoftKeyboard());
+        onView(withId(R.id.passInp)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.passConfirmInp)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.loginbut2)).perform(click());
+
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+
+        onView(withId(R.id.backbutton)).perform(click());
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+        onView(withId(R.id.signupbut1)).perform(click());
+        onView(withId(R.id.usernameInp)).perform(typeText(testUser), closeSoftKeyboard());
+        onView(withId(R.id.emailInp)).perform(typeText(email), closeSoftKeyboard());
+        onView(withId(R.id.passInp)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.passConfirmInp)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.loginbut2)).perform(click());
+
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+
+        onView(withText("Username or email already exists")).check(matches(isDisplayed()));
+        onView(withId(android.R.id.button1)).perform(click());
+
+    }
 
 }
