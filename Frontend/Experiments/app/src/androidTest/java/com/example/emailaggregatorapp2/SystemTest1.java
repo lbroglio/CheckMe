b232b2eeb88d39822a8315c65d65fc77e97f924c
josh <josh@rarceo.com>
2023-12-06 20:59:42 -0600

    Sign up test started

--- Frontend/Experiments/app/src/androidTest/java/com/example/emailaggregatorapp2/SystemTest1.java
+++ Frontend/Experiments/app/src/androidTest/java/com/example/emailaggregatorapp2/SystemTest1.java
@@ -1,5 +1,6 @@
 package com.example.emailaggregatorapp2;
 
+
 import com.example.emailaggregatorapp2.MainActivity;
 
 import org.junit.Before;
@@ -8,20 +9,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
+
 import androidx.test.filters.LargeTest;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.StringEndsWith.endsWith;
-
-import android.support.test.rule.ActivityTestRule;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import androidx.test.rule.ActivityTestRule;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 @LargeTest
@@ -29,8 +30,40 @@ public class SystemTest1 {
     private static final int SIMULATED_DELAY_MS = 500;
 
 
-    @Rule   // needed to launch the activity
+    @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp(){
+        Intents.init();
+    }
+
+
+    @Test
+    public void signUpSuccess(){
+        //generate random username
+        String testUser = "testUser" + (int)(Math.random() * 1000);
+        String email = testUser + "@gmail.com";
+        String password = "password";
+
+
+
+        //input info into sign up page, then see if activity changes to login page or if an error message pops up
+        onView(ViewMatchers.withId(R.id.signupbut1)).perform(click());
+        onView(ViewMatchers.withId(R.id.usernameInp)).perform(typeText(testUser), closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.emailInp)).perform(typeText(email), closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.passInp)).perform(typeText(password), closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.passConfirmInp)).perform(typeText(password), closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.loginbut2)).perform(click());
+
+        try {
+            Thread.sleep(SIMULATED_DELAY_MS);
+        } catch (InterruptedException e) {
+        }
+
+        intended(hasComponent(LoginActivity.class.getName()));
+
+    }
 
 
 }
